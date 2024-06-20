3f6319b49b2a72e1612f3ab97be0253ad227b4a8
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-06-20 23:13:18 +0100

    Add all costumes in lobby in initial

--- contentScript.js
+++ contentScript.js
@@ -1,28 +1,33 @@
 console.log("contentScript.js loaded");
 
-// Initial Costume Apply
+// Initial Costume Apply on lobby
 const observer = new PerformanceObserver((list) => {
     list.getEntries().forEach((entry) => {
       if (entry.id === "cages_div"){
-        chrome.storage.local.get(["catId", "catImageSrc", "catImageSize"], function(result){
-            if (result.catId === undefined) return;
-            var catId = result.catId;
-            var catPos = document.getElementById("cages").querySelectorAll("a[href='/cat" + catId + "']")[0].closest(".cat").querySelector(".d")
+        HTMLCollection.prototype.forEach = Array.prototype.forEach;
+        document.getElementsByClassName("cat_tooltip").forEach(function (element) {
+            var catId = element.querySelector("a").getAttribute("href").replace("/cat", "");
+            var cat = element.closest(".cat")
+            var catPos = cat.querySelector(".d");
+            if (catPos == null) {
+                console.log("CatPos is null")
+                console.log(element)
+                return;
+            }
+            var catSize = cat.getElementsByClassName("first")[0].style.backgroundSize;
             fetch("http://localhost:1300/search?name=" + catId)
             .then(response => response.json())
             .then(data => {
+                if (data["children"].length == 0) return;
                 var costumeURL = "http://localhost:1300/images/" + data["children"][0]["imguuid"] + ".png";
                 console.log("URL: " + costumeURL)
                 var costume = document.createElement("div");
                 costume.setAttribute("data-v-59afe5e8", "");
-                costume.style.backgroundSize = result.catImageSize;
+                costume.style.backgroundSize = catSize;
                 costume.style.backgroundImage = "url('"+ costumeURL +"')"
                 costume.style.position = "absolute";
                 catPos.appendChild(costume);
             })
-            .catch(error => {
-                console.error('There was a problem with your fetch operation:', error);
-            });
         });
       }
     });
@@ -39,6 +44,7 @@ const observer2 = new MutationObserver((mutations) => {
                     // A cat has been added
                     var catId = node.querySelector("a").getAttribute("href").replace("/cat", "");
                     var catPos = node.querySelector(".d");
+                    var catsize = node.getElementsByClassName("first")[0].style.backgroundSize;
                     fetch("http://localhost:1300/search?name=" + catId)
                     .then(response => response.json())
                     .then(data => {
@@ -47,7 +53,7 @@ const observer2 = new MutationObserver((mutations) => {
                         console.log("URL: " + costumeURL)
                         var costume = document.createElement("div");
                         costume.setAttribute("data-v-59afe5e8", "");
-                        costume.style.backgroundSize = "cover";
+                        costume.style.backgroundSize = catsize;
                         costume.style.backgroundImage = "url('"+ costumeURL +"')"
                         costume.style.position = "absolute";
                         catPos.appendChild(costume);
