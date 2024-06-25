5b8e819f465b330de874f36bb20dcaf96e4092fd
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-06-25 18:51:10 +0100

    Apply costume after sniff action

--- contentScript.js
+++ contentScript.js
@@ -41,9 +41,9 @@ if (window.location.href.includes("https://catwar.su/cw3")) {
     // Costume Apply on Cage Change
     const observer2 = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
+            // console.log(mutation)
             if (mutation.type === "childList") {
                 mutation.addedNodes.forEach((node) => {
-                    // console.log(node)
                     if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains("catWithArrow")){
                         // A cat has been added
                         var catId = node.querySelector("a").getAttribute("href").replace("/cat", "");
@@ -63,9 +63,24 @@ if (window.location.href.includes("https://catwar.su/cw3")) {
                             catPos.appendChild(costume);
                         })
                     }
-                    else if (node.nodeType === Node.ELEMENT_NODE && node.id === "cages_div") {
-                        // Scenary Change
-                        console.log("Scenary Change")
+                    else if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains("cage_items") && node.querySelector(".d") != null){
+                        // A cat has been added after sniff
+                        var catId = node.querySelector("a").getAttribute("href").replace("/cat", "");
+                        var catPos = node.querySelector(".d");
+                        var catsize = node.querySelector(".first").style.backgroundSize;
+                        fetch("http://localhost:1300/search?name=" + catId)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data["children"].length == 0) return;
+                            var costumeURL = "http://localhost:1300/images/" + data["children"][0]["imguuid"] + ".png";
+                            console.log("URL: " + costumeURL)
+                            var costume = document.createElement("div");
+                            costume.setAttribute("data-v-59afe5e8", "");
+                            costume.style.backgroundSize = catsize;
+                            costume.style.backgroundImage = "url('"+ costumeURL +"')"
+                            costume.style.position = "absolute";
+                            catPos.appendChild(costume);
+                        })
                     }
                 });
             }
