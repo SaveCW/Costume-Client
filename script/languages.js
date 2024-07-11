c0b481cab2f2387d7505309977644ccce0f4b2b1
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-07-11 19:24:47 +0100

    Make sure everything has loaded first

--- script/languages.js
+++ script/languages.js
@@ -1,100 +1,105 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    document.getElementsByClassName('dropbtn')[0].classList.toggle("dropdown-open")
-}
 
-document.getElementsByClassName("dropbtn")[0].addEventListener("click", myFunction);
-  
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        const dropBtn = document.getElementsByClassName('dropbtn')[0];
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-            dropBtn.classList.remove("dropdown-open")
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementsByClassName('dropbtn')[0].classList.toggle("dropdown-open")
+    }
+
+    document.getElementsByClassName("dropbtn")[0].addEventListener("click", myFunction);
+    
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            const dropBtn = document.getElementsByClassName('dropbtn')[0];
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                dropBtn.classList.remove("dropdown-open")
+            }
+            }
         }
     }
-}
 
-document.getElementById("russian").addEventListener("click", function() {
-    chrome.storage.local.set({"language": "ru"});
-    location.reload();
-});
-
-document.getElementById("english").addEventListener("click", function() {
-    chrome.storage.local.set({"language": "en"});
-    location.reload();
-});
-
-function changeText(data){
-    if (document.getElementById("title") != null) 
-        document.getElementById("title").innerHTML = data["title"];
-    if (document.getElementById("catTitle") != null)
-        document.getElementById("catTitle").innerText = data["catTitle"];
-    if (document.getElementById("getID") != null)
-        document.getElementById("getID").innerText = data["getID"];
-    if (document.getElementById("changeTitle") != null)
-        document.getElementById("changeTitle").innerText = data["changeTitle"];
-    if (document.getElementById("note") != null)
-        document.getElementById("note").innerText = data["note"];
-    if (document.getElementById("changeCostume") != null)
-        document.getElementById("changeCostume").innerText = data["changeCostume"];
-    if (document.getElementById("dpopupContent") != null)
-        document.getElementById("dpopupContent").innerText = data["dpopupContent"];  
-}
-
-function translateText(lang) {
-    fetch("./lang/" + lang + ".json")
-    .then(response => response.json())
-    .then(data => {
-        changeText(data);
-        // Save on localstorage
-        chrome.storage.local.set({"langdata": data});
+    document.getElementById("russian").addEventListener("click", function() {
+        chrome.storage.local.set({"language": "ru"});
+        location.reload();
     });
 
-    // Send request to contentScript to change the text
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {message:"languageChange", language:lang}, function(response) {
-            // console.log(response);
+    document.getElementById("english").addEventListener("click", function() {
+        chrome.storage.local.set({"language": "en"});
+        location.reload();
+    });
+
+    function changeText(data){
+        if (document.getElementById("title") != null) 
+            document.getElementById("title").innerHTML = data["title"];
+        if (document.getElementById("catTitle") != null)
+            document.getElementById("catTitle").innerText = data["catTitle"];
+        if (document.getElementById("getID") != null)
+            document.getElementById("getID").innerText = data["getID"];
+        if (document.getElementById("changeTitle") != null)
+            document.getElementById("changeTitle").innerText = data["changeTitle"];
+        if (document.getElementById("note") != null)
+            document.getElementById("note").innerText = data["note"];
+        if (document.getElementById("changeCostume") != null)
+            document.getElementById("changeCostume").innerText = data["changeCostume"];
+        if (document.getElementById("dpopupContent") != null)
+            document.getElementById("dpopupContent").innerText = data["dpopupContent"];  
+    }
+
+    function translateText(lang) {
+        fetch("./lang/" + lang + ".json")
+        .then(response => response.json())
+        .then(data => {
+            changeText(data);
+            // Save on localstorage
+            chrome.storage.local.set({"langdata": data});
         });
-    });
-}
+        
+        // Send request to contentScript to change the text
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log(tabs[0])
+            if (tabs[0].status === "complete"){
+                chrome.tabs.sendMessage(tabs[0].id, {message:"languageChange", language:lang}, function(response) {
+                    // console.log(response);
+                });
+            }
+        });
+    }
 
 
-// Get current language picked
-chrome.storage.local.get("language", function(result) {
-    var language = result.language;
+    // Get current language picked
+    chrome.storage.local.get("language", function(result) {
+        var language = result.language;
 
-    switch (language) {
-        case "ru":
-            document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/russian.png";
-            translateText("ru");
-            break;
-        case "en":
-            document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/english.png";
-            translateText("en");
-            break;
-        default:
-            // Check default language
-            const defaultLanguage = navigator.language;
-            if (defaultLanguage.includes("ru")) {
+        switch (language) {
+            case "ru":
                 document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/russian.png";
                 translateText("ru");
-                chrome.storage.local.set({"language": "ru"});
-            } else {
+                break;
+            case "en":
                 document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/english.png";
                 translateText("en");
-                chrome.storage.local.set({"language": "en"});
-            }
-            break;
-    }
+                break;
+            default:
+                // Check default language
+                const defaultLanguage = navigator.language;
+                if (defaultLanguage.includes("ru")) {
+                    document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/russian.png";
+                    translateText("ru");
+                    chrome.storage.local.set({"language": "ru"});
+                } else {
+                    document.getElementsByClassName("dropbtn")[0].querySelector("img").src = "./images/english.png";
+                    translateText("en");
+                    chrome.storage.local.set({"language": "en"});
+                }
+                break;
+        }
+    });
 });
-
 
