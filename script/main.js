84cbb6137587a94802faae6dedfeb5191ab04921
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-07-16 18:43:59 +0100

    Improvements + more functionality additions
    + Adds new errors and design
    + ensures inputs are correctly added and are not empty or invalid
    + Moves to next stage for number verification
    + Complete redesign and addition of prototype for code verification
    + Fix for height problem not calibrating and shifting text up above the original height

--- script/main.js
+++ script/main.js
@@ -10,10 +10,50 @@ document.getElementById("getInfo").addEventListener("click", function() {
     });
 });
 
+function setError(message, color){
+    var status = document.getElementsByClassName("statusContainer")[0];
+    if (status.querySelector("#status") != null) {
+        status.removeChild(status.querySelector("#status"));
+    }
+
+    var div = document.createElement("div");
+    div.id = "status";
+    div.style.backgroundColor = color;
+    div.innerText = message;
+    status.appendChild(div);
+
+    // Resize popup
+    document.body.style.height = "fit-content";
+
+    setTimeout(() => {
+        div.style.opacity = 0;
+        setTimeout(() => {
+            div.style.backgroundColor = "transparent";
+            div.innerHTML = "";
+        }, 2000);
+    }, 2000);
+}
+
 document.getElementById("submit").addEventListener("click", function() {
     var id = document.getElementById("id").value;
     var username = document.getElementById("username").value;
+    var status = document.getElementsByClassName("statusContainer")[0];
     var data = { id: id, username: username };
+
+    if (id == "" || username == "") {
+        setError("Please fill in all the fields", "red");
+        return;
+    }
+
+    // If id is not only numbers
+    if (!/^\d+$/.test(id)) {
+        setError("ID must be only numbers", "red");
+        return;
+    }
+
+    if (status.querySelector("#status") != null) {
+        status.removeChild(status.querySelector("#status"));
+    }
 
     fetch("http://localhost:1300/register", {
         method: 'POST',
@@ -32,16 +72,76 @@ document.getElementById("submit").addEventListener("click", function() {
     div.className = "form-group"
 
     var label = document.createElement("label");
-    label.innerText = "ID: " + id;
+    label.innerHTML = "We have send a <a href='https://catwar.su/ls'>PM</a> to your account please enter the 6 digit code below";
     div.appendChild(label);
 
-    var div_name = document.createElement("div");
-    div_name.className = "form-group"
+    var codeContainer = document.createElement("div");
+    codeContainer.className = "codeContainer";
 
-    var label_name = document.createElement("label");
-    label_name.innerText = "Username: " + username;
-    div_name.appendChild(label_name);
+    for (var i = 0; i < 6; i++) {
+        var input = document.createElement("input");
+        input.type = "number";
+        input.id = "code" + i;
+        input.className = "code";
+        input.placeholder = "0";
+        // Add an event listener to restrict input length
+        input.onpaste = e => e.preventDefault();
 
+        input.addEventListener('input', function() {
+            if (this.value.length > 1) {
+                this.value = this.value.slice(0, 1); // Keep only the first digit
+
+            }
+            // Go to next input
+            var next = this.nextElementSibling;
+            if (next) {
+                next.focus();
+                next.select();
+            }
+            else {
+                // Go to start input
+                var start = this.parentElement.firstElementChild;
+                start.focus();
+                start.select();
+            }
+        });
+        codeContainer.appendChild(input);
+    }
+    div.appendChild(codeContainer);
     field.appendChild(div);
-    field.appendChild(div_name);
+
+    var button_div = document.createElement("div");
+    button_div.className = "form-group";
+
+    var button = document.createElement("button");
+    button.innerText = "Submit";
+    button.id = "submitCode";
+
+    button_div.appendChild(button);
+
+    field.appendChild(button_div);
+
+
+    // Listener for code
+    document.getElementById("submitCode").addEventListener("click", function() {
+        var code = document.getElementById("code").value;
+        var data = { id: id, username: username, code: code };
+
+        if (code == "") {
+            setError("Please enter the code", "red");
+            return;
+        }
+
+        fetch("http://localhost:1300/verify", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+
+    });
+
+
 });
