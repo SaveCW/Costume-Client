760e4726895504469cd800f6d4c23c09fe7b6159
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-01-26 13:04:50 +0000

    Initial addition of basic elements

---
+++ script/frontSettings.js
@@ -0,0 +1,7 @@
+document.getElementById('settingsButton').addEventListener('click', function() {
+    document.getElementById('settingsLayer').style.display = 'block';
+});
+
+document.getElementById('closeSettings').addEventListener('click', function() {
+    document.getElementById('settingsLayer').style.display = 'none';
+});
